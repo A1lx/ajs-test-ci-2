e604e4ce0e21167e1552d970f79591ba0112fdae
Aleksey Klimov <alx111wrk@gmail.com>
2023-03-24 23:31:54 +0300

    initial commit

---
+++ src/__tests__/test.js
@@ -0,0 +1,16 @@
+import showPlayers from '../players';
+
+test('testing showPlayers func', () => {
+  const playersList = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const resultArr = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  const result = showPlayers(playersList);
+  expect(result).toEqual(resultArr);
+});
